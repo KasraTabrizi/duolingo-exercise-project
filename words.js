164a9b100214c3ac5c0ad5611c54210eab57a21a
Kasra Tabrizi <kasra_tabrizi@hotmail.com>
2022-03-31 18:42:16 +0200

    create exercise list

---
+++ words.js
@@ -0,0 +1,17 @@
+const exercises = [
+  {
+    english: "I eat an apple",
+    spanish: "Yo como una manzana",
+    list: ["manazana", "comes", "Yo", "una", "como", "naranja"],
+  },
+  {
+    english: "This is my wife",
+    spanish: "Este es mi esposa",
+    list: ["Este", "esta", "esposa", "es", "mi", "hermana"],
+  },
+  {
+    english: "My favorite sport is hockey",
+    spanish: "Mi deporte favorito es hockey",
+    list: ["deporte", "favorito", "hockey", "Mi", "es", "Tu"],
+  },
+];
